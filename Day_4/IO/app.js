218821e3caf362e6515f974555458f10377feb9e
Seth Shober <seth.shober@gmail.com>
2015-01-09 16:31:35 -0800

    show read time

--- Day_4/IO/app.js
+++ Day_4/IO/app.js
@@ -7,11 +7,21 @@ var args = process.argv.slice(2);
 
 function showFileContents (fileArr) {
 
+	function time (name) {
+	  var start = Date.now();
+	  return function() {
+	    console.log('time %s: %dms', name, Date.now() - start);
+	  };
+	};
+
+	var done = time('reading file');
+
 	for ( var i = 0; i < fileArr.length; i++ ) {
 
 		fs.readFile(fileArr[i], { encoding: 'utf8' }, function(error, contents) {
 			if (error) console.error(error.message);
 	  		console.log(contents);
+	  		done();
 		});
 
 	}
