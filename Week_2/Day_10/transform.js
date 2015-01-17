e5c2bbd1d110009d0e603579a4b40510524adbc6
Seth Shober <seth.shober@gmail.com>
2015-01-16 19:56:55 -0800

    comments and slight change to conditional

--- Week_2/Day_10/transform.js
+++ Week_2/Day_10/transform.js
@@ -1,21 +1,17 @@
 // We created a 'transform' function as a method to the Array prototype. It takes an array and returns an array of values determined by typeof parameter.
 // Function will iterate over array that method is run on and return a new array with values matching the typeof value you passed as a parameter.
-// arg type takes a typeof of parameter that you want for returned values. strict is optional and is true or false.
-// If strict is true passing 'object' will return only objects minus arrays. Where as false will return objects and arrays by passing 'object'.
-// Example: transformArray.myTransformFunction will return a new array with all objects in transfromArray.
+// argument 'type' takes a typeof of parameter that you want for returned values. 'strict' is optional and is true or false.
+// If 'strict' is true, passing 'object' as typeof will return only objects minus arrays. Where as false will return objects and arrays by passing 'object'. The 'strict' argument is optional and will default to false.
+// Example: transformArray.myTransformFunction('string') will return a new array with all strings in transformArray.
 
 var transformArray = [ [1,2,3,4,5], 100, "world", {name: "Seth", home: "Portland"}, "hello", 50, undefined, null, true, false, ["values", "more values"], { key:"value" } ];
 Array.prototype.myTransformFunction = function(type, strict){
 
 	var newArr = [];
 	this.filter(function(item){
-		if ( type === "array") {
+		if ( type === "array" && Array.isArray(item)) { newArr.push(item); }	
 
-			if ( Array.isArray(item) ) { newArr.push(item); }
-
-		}	
-
-		if ( type === "object" && strict) {
+		if ( type === "object" && strict ) {
 
 			if ( typeof item === type && ! Array.isArray(item) ) { newArr.push(item); }
 	
