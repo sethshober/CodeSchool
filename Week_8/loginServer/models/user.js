fd7141a5e54aae92c29632f3de61133e75ae6cfa
Seth Shober <seth.shober@gmail.com>
2015-03-02 15:48:37 -0800

    inital server setup

---
+++ Week_8/loginServer/models/user.js
@@ -0,0 +1,144 @@
+var mongoose = require('mongoose');
+var crypto   = require('crypto'); // leaving this commented until everything else is working.
+var Schema   = mongoose.Schema;
+
+// User Schema
+var UserSchema = new Schema({
+	name: String,
+	email: {
+		type: String,
+		trim: true,
+		unique: true
+	},
+	// username: {
+	// 	type: String,
+	// 	trim: true,
+	// 	unique: true // guarantee no duplicate values
+	// },
+	password: {
+		type: String,
+		min: 8
+	},
+	salt: {
+		type: String
+	}
+});
+
+// User Class Setup
+var User = mongoose.model('User', UserSchema);
+
+
+
+
+// CREATE PRE-SAVE MIDDLEWARE TO HANDLE THE USERS' PASSWORD HASHING. REPLACES CURRENT PASSWORD WITH A HASHED ONE. SALTS SHOULD BE ADDED IN FUTURE.
+UserSchema.pre('save', function (next) {
+		var user = this;
+
+		if (user.password) {
+			var salt = createSalt();
+			var sha = crypto.createHash('sha256');
+			user.salt = salt;
+			user.password = sha.update(this.password).digest('hex');
+		}
+		next();
+	}
+);
+
+
+// WE'VE ADDED AN AUTHENTICATION METHOD WHICH ACCEPTS A STRING PASSWORD ARGUMENT, WHICH IT THEN HASHES AND COMPARES TO THE CURRENT USERS HASHED PASSWORD.
+UserSchema.methods.authenticate = function (password) {
+	var user = this;
+	var sha = crypto.createHash('sha256');
+	sha = sha.update(password).digest('hex');
+	return user.password === sha;
+}
+
+
+// USED TO FIND AN AVAILABLE UNIQUE USERNAME FOR NEW USERS (FOR OAUTH)
+// UserSchema.statics.findUniqueUsername = function (username, suffix, callback) {
+// 	var _this = this;
+// 	var possibleUsername = username + (suffix || '');
+
+// 	_this.findOne(
+// 		{username: possibleUsername},
+// 		function(err, user) {
+// 			if (!err) {
+// 				if (!user) {
+// 					callback(possibleUsername);
+// 				}
+// 				else {
+// 					return _this.findUniqueUsername(username, (suffix || 0) + 1, callback);
+// 				}
+// 			}
+// 			else {
+// 				callback(null);
+// 			}
+// 		}
+// 	);
+// };
+
+
+// create a secure random 32 byte (256 bit) hex string
+function createSalt() {
+	crypto.randomBytes(32, function (ex, buf) {
+		if (ex) throw ex;
+		var salt = buf.toString('hex');
+		console.log(salt);
+		return salt;
+	});
+}
+
+
+
+// SAVE CREATED USER
+function saveUser (newUser, callback) {
+
+	User.update(newUser, function(error){
+		if (error) { return console.error(error); }
+		else { callback(); }
+	});
+
+};
+    
+
+
+// UPDATE USER DATA
+function updateUser (oldDataObj, newDataObj) {
+
+	User.update(oldDataObj, newDataObj, function(err){
+		if (err) { return console.error(err); }
+	}); 
+
+};
+
+
+
+// DELETE USER FROM DATABASE
+function deleteUser (dataObj) {
+
+	User.remove(dataObj, function(err){
+		if (err) { return console.error(err); }
+	});
+
+};
+
+
+function findUser (email, callback) {
+
+	User.findOne({"email": email}, function(err, user){
+		console.log(user);
+		if (err) { return console.error(err); }
+		else { callback(user) }
+	});
+
+};
+
+
+
+module.exports = {
+	User: User,
+	saveUser: saveUser,
+	updateUser: updateUser,
+	deleteUser: deleteUser,
+	findUser: findUser
+};
---
+++ Week_8/loginServer/routes/index.js
@@ -0,0 +1,108 @@
+var express = require('express');
+var router  = express.Router();
+var User    = require('../models/user.js');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  if (!req.cookies["youAreLoggedIn"]) {
+  	res.redirect('/login');
+  }	else {
+  	res.render('index', {title: 'Just Because Login'});
+  }	
+});
+
+
+/* POST home page. */
+router.post('/', function(req, res, next) {
+  Post.savePost(newPost);
+    //res.redirect('/');
+});
+
+
+
+router.get('/posts', function(req, res, next) {
+	Post.getPosts(function(posts){
+		var postHTML;
+		for ( var i = 0; i < posts.length; i++ ) {
+			postHTML += "<div class='post'><p class='text-center post-content'>" + posts[i].content + "</p><span class='hearted glyphicon glyphicon-heart'></span></div>";
+		}
+		console.log(postHTML);
+		res.json(postHTML);
+	});
+});
+
+
+
+/* GET logout page */
+router.get('/logout', function(req, res, next) {
+  res.clearCookie("youAreLoggedIn");
+  res.redirect('/');
+});
+
+/* GET login page */
+router.get('/login', function(req, res, next) {
+  if (!req.cookies["youAreLoggedIn"]) {
+  	res.render('login', {title: "Log In You!"});
+  }	else {
+  	res.redirect('/');
+  }	
+});
+
+/* POST login page */
+router.post('/login', function(req, res, next) {
+	console.log(req.body);
+    User.findUser(req.body.email, function(user) {
+    	//var user = user[0];
+    	console.log(user);
+	    if ( user && user.email === req.body.email && user.password === req.body.password ) {
+	   	  console.log("success");
+	      res.cookie('youAreLoggedIn', user._id, { maxAge: 2592000000 });
+	      res.redirect('/');
+	    } else { 
+        console.log("failed"); res.render ('login', {title: 'Incorrect login information'}); 
+      }
+    });
+
+});
+
+
+router.get('/signup', function(req, res, next) {
+  if (!req.cookies["youAreLoggedIn"]) {
+    res.render('signup', {title: "Sign up You!"});
+  } else {
+    res.redirect('/');
+  } 
+});
+
+
+router.post('/signup', function(req, res, next) {
+  console.log("signup received");
+  User.saveUser(req.body, function(){
+    User.findUser(req.body.email, function(user){
+      console.log(user);
+      res.cookie('youAreLoggedIn', user._id, { maxAge: 2592000000 });
+      res.redirect('/');
+    });
+
+  });
+
+});
+
+
+
+router.get('/test', function(req, res, next) {
+  console.log(User.findUser("hello@hello.com"));
+})
+
+
+
+
+
+
+
+
+module.exports = router;
+
+
+
+
